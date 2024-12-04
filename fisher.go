645101f9ede660b7483d6651496bc1903bf29d59
jaumefe <jaumef.escrig96@gmail.com>
2024-12-04 20:54:25 +0100

    FisherYatesShuffle now admits options to configure seed and if there are exclude indices

--- fisher.go
+++ fisher.go
@@ -2,21 +2,40 @@ package stats
 
 import (
 	"math/rand"
+	"time"
 )
 
-func FisherYatesShuffleWithExclusion(arr []any, excludeIndices map[int]bool, seed int64) {
+type ShuffleOptions struct {
+	Seed           int64
+	ExcludeIndices map[int]bool
+}
+
+func FisherYatesShuffle(arr []any, opts *ShuffleOptions) {
+	seed := time.Now().UnixNano()
+	var excludeIndices map[int]bool
+
+	if opts != nil {
+		if opts.Seed != 0 {
+			seed = opts.Seed
+		}
+
+		if opts.ExcludeIndices != nil {
+			excludeIndices = opts.ExcludeIndices
+		}
+	}
+
 	r := rand.New(rand.NewSource(seed))
 	n := len(arr)
 
 	validIndices := make([]int, 0, n)
 	for i := 0; i < n; i++ {
-		if !excludeIndices[i] {
+		if excludeIndices == nil || !excludeIndices[i] {
 			validIndices = append(validIndices, i)
 		}
 	}
 
 	for i := n - 1; i > 0; i-- {
-		if !excludeIndices[i] {
+		if excludeIndices == nil || !excludeIndices[i] {
 			j := validIndices[r.Intn(len(validIndices))]
 			arr[i], arr[j] = arr[j], arr[i]
 		}
--- fisher_test.go
+++ fisher_test.go
@@ -5,16 +5,16 @@ import (
 	"testing"
 )
 
-func TestFisherYatesExclusion(t *testing.T) {
+func TestFisherYatesShuffleWithExclusion(t *testing.T) {
 	input := []any{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
-	excludedInd := map[int]bool{1: true, 2: true, 7: true}
 	expected := []any{6, 1, 2, 8, 0, 5, 4, 7, 9, 3}
+	opts := &ShuffleOptions{Seed: int64(3), ExcludeIndices: map[int]bool{1: true, 2: true, 7: true}}
 
 	// Copying original input
 	shuffled := make([]any, len(input))
 	copy(shuffled, input)
 
-	FisherYatesShuffleWithExclusion(shuffled, excludedInd, 3)
+	FisherYatesShuffle(shuffled, opts)
 
 	// Check if the result matches with the expected one
 	if !reflect.DeepEqual(shuffled, expected) {
@@ -22,10 +22,43 @@ func TestFisherYatesExclusion(t *testing.T) {
 	}
 
 	// Checking that exclusions has not moved
-	for i := range excludedInd {
+	for i := range opts.ExcludeIndices {
 		if shuffled[i] != input[i] {
 			t.Errorf("Excluded index %d was altered: expected %v, got %v", i, input[i], shuffled[i])
 		}
+	}
+
+	// Checking that all elements are conserved
+	originalMap := make(map[any]int)
+	shuffledMap := make(map[any]int)
+
+	for _, in := range input {
+		originalMap[in]++
+	}
+
+	for _, sh := range shuffled {
+		shuffledMap[sh]++
+	}
+
+	if !reflect.DeepEqual(originalMap, shuffledMap) {
+		t.Errorf("Mismatch in elements: original %v, shuffled %v", originalMap, shuffledMap)
+	}
+}
+
+func TestFisherYatesShuffle(t *testing.T) {
+	input := []any{0, 1, 2, 3, 4, 5, 6, 7, 8, 9}
+	opts := &ShuffleOptions{Seed: int64(3)}
+	expected := []any{9, 8, 5, 0, 1, 6, 3, 2, 7, 4}
+
+	// Copying original input
+	shuffled := make([]any, len(input))
+	copy(shuffled, input)
+
+	FisherYatesShuffle(shuffled, opts)
+
+	// Check if the result matches with the expected one
+	if !reflect.DeepEqual(shuffled, expected) {
+		t.Errorf("Expected %v, got %v", expected, shuffled)
 	}
 
 	// Checking that all elements are conserved
